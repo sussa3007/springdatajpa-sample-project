1a0d6d22b825af12deabac269a7aac839f5109ee
sussa3007 <sussa1933@gmail.com>
2022-10-27 21:04:48 +0900

    Set #1 - Project Set
    # H2 DB 셋팅 및 테스트
    # 외부 라이브러리 추가
    # 웰컴페이지 와 타임리프 동작 확인 테스트

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMemer() {
+        // Given
+        Member member = new Member();
+        member.setUsername("mamberA");
+        // When
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // Then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        /* 같은 객체로 인식한다. 하나의 트랜잭션 안에서 조회하는 것이기 때문에- 영속성*/
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+}
